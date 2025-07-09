e4ddd48a3b2f6cd3086fc76a270f5ac2ddc75b35
Brahim <matermihach@gmail.com>
2025-07-09 09:05:46 +0200

    fix: updated generate-code and related files

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyChpbptd5H6oxYT5CuPPFMcx3-hRXQ9z08",                 
   authDomain: "matermihach-44888.firebaseapp.com",
   projectId: "matermihach-44888",
-  storageBucket: "matermihach-44888.appspot.com",
+  storageBucket: "matermihach-44888.firebasestorage.app",
   messagingSenderId: "1042809307063",
   appId: "1:1042809307063:web:2dd8046f3b8caca17b7232"
 };
